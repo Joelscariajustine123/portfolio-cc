e4923a0e06248eb7195c0ffafb4b93a165f919ad
Joel scaria Justine <84953776+Joelscariajustine123@users.noreply.github.com>
2025-10-28 00:14:53 +0530

    change images

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -30,7 +30,7 @@ const skillsData: { category: string; skills: Skill[] }[] = [
                 name: 'Nmap', 
                 icon: (
                     <img 
-                        src="/img/nmap.png" 
+                        src="../img/nmap.png" 
                         alt="Nmap" 
                         className="w-6 h-6 object-contain" 
                     />
@@ -41,7 +41,7 @@ const skillsData: { category: string; skills: Skill[] }[] = [
                 name: 'Nessus', 
                 icon: (
                     <img 
-                        src="/img/nessus.png" 
+                        src="../img/nessus.png" 
                         alt="Nessus" 
                         className="w-6 h-6 object-contain" 
                     />
